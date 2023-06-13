10bfd01dfa086d4e6621e520b691f4f178a67182
amine <bouramtaneamine1@gmail.com>
2023-06-13 20:51:42 +0100

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="container">
-      <h1>hey from bootstrap</h1>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
